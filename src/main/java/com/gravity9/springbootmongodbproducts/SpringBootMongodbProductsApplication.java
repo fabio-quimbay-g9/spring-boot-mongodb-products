bbf06e35cd4b6067caf101481fa73cc475e330f8
Fabio Quimbay <fabio@MacBook-Pro-de-Fabio.local>
2023-12-23 20:28:33 -0500

    Initial commit

---
+++ src/main/java/com/gravity9/springbootmongodbproducts/SpringBootMongodbProductsApplication.java
@@ -0,0 +1,13 @@
+package com.gravity9.springbootmongodbproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gravity9/springbootmongodbproducts/SpringBootMongodbProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gravity9.springbootmongodbproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
